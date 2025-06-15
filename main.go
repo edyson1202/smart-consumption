88f050868d2e164f6ffceb14793c04ff653affac
Boris <b.edyson.12@gmail.com>
2025-06-15 22:07:36 +0300

    allow credentials

--- main.go
+++ main.go
@@ -60,6 +60,7 @@ func enableCORS(next http.HandlerFunc) http.HandlerFunc {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		// Handle preflight OPTIONS request
 		if r.Method == http.MethodOptions {
